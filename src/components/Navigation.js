c31f4ea022299c477c209461340781e2bb81be1c
Holylogus <hamar.patrik94@gmail.com>
2024-08-23 13:52:34 +0200

    ProjectStart

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import { Navbar } from 'react-bootstrap';
+
+
+
+export default function Navigation(){
+
+    return(
+        <Navbar expand="lg" bg="dark" variant="light">
+           
+        </Navbar>
+    )
+}
