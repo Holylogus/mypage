9eea534ae74607d8cf6c8498acc5d68a26bdfea4
Holylogus <hamar.patrik94@gmail.com>
2024-08-26 12:27:29 +0200

    Navigation and CarouselSection create

--- src/components/CarouselContainer.js
+++ src/components/CarouselContainer.js
@@ -1,7 +1,51 @@
+import { Carousel } from 'react-bootstrap';
+import c1 from "../images/c1.png"
+import c2 from "../images/c2.png"
+import c3 from "../images/c3.png"
+import c4 from "../images/c4.png"
+import c5 from "../images/c5.png"
+import "../styles/CarouselContainer.css"
+
 export default function CarouselContainer() {
     return(
         <div id="CarouselContainer">
-
+            <Carousel>
+                <Carousel.Item>
+                <img
+                        className="d-block w-100 h-100"
+                        src={c1}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 h-100"
+                        src={c2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 h-100"
+                        src={c3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 h-100"
+                        src={c4}
+                        alt="Fourth slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 h-100"
+                        src={c5}
+                        alt="Fifth slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,29 @@
-import { Navbar, Nav, } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Button} from 'react-bootstrap';
 import "../styles/Navigation.css";
 
 
 export default function Navigation(){
 
     return(
+        <div id='Navigation'>
         <Navbar expand="lg" bg="dark" variant="dark">
            <Navbar.Brand>
-            HD Garage
+            <Nav.Link href="/">HD Gargae</Nav.Link>
            </Navbar.Brand>
-           <Nav.Link href="/login"><span>Login</span></Nav.Link>
+           <NavDropdown title="Szervíz" id="nav-dropdown" menuVariant='dark'>
+            <NavDropdown.Item eventKey="4.1">Autószervíz</NavDropdown.Item>
+            <NavDropdown.Item eventKey="4.2">Motorgenerál</NavDropdown.Item>
+            <NavDropdown.Item eventKey="4.3">Diagnosztika</NavDropdown.Item>
+            <NavDropdown.Item eventKey="4.4">Karbantartás</NavDropdown.Item>
+           </NavDropdown>
+           <Nav.Item>
+            Kapcsolat
+           </Nav.Item>
+           <Button variant="danger" onClick={() => console.log("Primary")}>
+            Bejelentkezés
+           </Button>
            
         </Navbar>
+        </div>
     )
 }
