75ff461ce629a11181193fe8bdb0c8c2d612fa23
Holylogus <hamar.patrik94@gmail.com>
2024-08-26 13:50:53 +0200

    create MenuSection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,3 +1,5 @@
+import "../styles/AboutSection.css"
+
 export default function AboutSection(){
 
     return(
--- src/components/MenuSection.js
+++ src/components/MenuSection.js
@@ -1,7 +1,41 @@
-export default function MenuSection() {
-    return(
-        <div id="MenuSection">
+import "../styles/MenuSection.css";
+import { Card } from 'react-bootstrap';
+import engineIcon from "../icons/car-engine.png";
+import carService from "../icons/service.png";
+import carRepair from "../icons/car-repair.png";
+import carDiag from "../icons/diagnostic.png";
 
-        </div>
-    )
+export default function MenuSection() {
+  return (
+    <div id="MenuSection">
+      <Card>
+        <Card.Img variant="top" src={carRepair} />
+        <Card.Body>
+          <Card.Title>Szervíz</Card.Title>
+          <Card.Text>Autószerelés márkától függetlenűl</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={engineIcon} />
+        <Card.Body>
+          <Card.Title>Motorgenerál</Card.Title>
+          <Card.Text>Motor teljeskörű felújítása Garanciával!</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={carDiag} />
+        <Card.Body>
+          <Card.Title>Diagnosztika</Card.Title>
+          <Card.Text>Megtaláljuk a hibakód okát</Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={carService} />
+        <Card.Body>
+          <Card.Title>Karbantartás</Card.Title>
+          <Card.Text>Elvégezzük atutója idószakos karbantartását</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
