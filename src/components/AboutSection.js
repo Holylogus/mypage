b1212c541419aa5f3aacbb036dbeccc6aec5ca66
Holylogus <hamar.patrik94@gmail.com>
2024-08-26 14:26:42 +0200

    ContractSection creating

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,10 +1,21 @@
-import "../styles/AboutSection.css"
+import "../styles/AboutSection.css";
+import { Button } from 'react-bootstrap';
 
-export default function AboutSection(){
-
-    return(
-        <div id="AboutSection">
-            
-        </div>
-    )
+export default function AboutSection() {
+  return (
+    <div id="AboutSection">
+      <h1>Üdvözlünk a BMW Szakszervíz Világában!</h1>
+      <h2>Kiváló Minőségű BMW Szerviz Tapasztalat</h2>
+      <p>
+        Üdvözöljük a HDGarage weboldalán, ahol a BMW szerelmesei találkoznak a
+        minőségi autószerviz szolgáltatásokkal. Cégünk elhivatott a BMW járművek
+        karbantartásában és javításában, és büszkén kínáljuk a legmagasabb
+        szintű szerviz tapasztalatot.
+      </p>
+      <p><strong>Miért Válasszon Minket?</strong></p>
+      <Button variant="danger" onClick={() => console.log("Primary")}>
+        Ajánlatkérés
+      </Button>
+    </div>
+  );
 }
--- src/components/ContractSection.js
+++ src/components/ContractSection.js
@@ -1,7 +1,30 @@
 export default function ContractSection() {
-    return(
-        <div id="ContractSection">
-            
-        </div>
-    )
+  return (
+    <div id="ContractSection">
+      <h1>HD Garage</h1>
+      <h2>Elérhetőségünk</h2>
+      <p>2363 Felsőpakony, Ipari út 1.</p>
+      <p>
+        <strong>+36 30 389 9145</strong>
+      </p>
+      <h2>Szervíz nyitvatartása</h2>
+      <p>
+        Htéfő - Szombat: <strong>8:00 - 18:00</strong>
+      </p>
+      <div className="PublicMediaContainer">
+        <a
+          class="p-4 m-3"
+          href="https://www.facebook.com/profile.php?id=100090888117425"
+        >
+          <img src="../icons/icons8-facebook-48.png" alt="facebbok" />
+        </a>
+        <a class="p-4 pb-3" href="/" >
+          <img src="../icons/icons8-instagram-48.png" alt="instagramm" />
+        </a>
+        <a class="p-4 pb-3" href="/">
+          <img src="../icons/icons8-tiktok-48.png" alt="tiktok" />
+        </a>
+      </div>
+    </div>
+  );
 }
